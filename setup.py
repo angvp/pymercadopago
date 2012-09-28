5e0996b3d8a8dd7615a9b4f07f14dbe786766d0f
Angel Velásquez <angvp@archlinux.org>
2012-09-28 05:50:01 -0300

    Adding setup for 0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ description = "A library to interact with the MercadoPago gateway payment"
 
 setup(
     name="pymercadopago",
-    version="0.0.2",
+    version="0.0.3",
     author="Angel Velasquez, Diego Ramirez",
     author_email="angel.velasquez@elo7.com, diego.ramirez@elo7.com",
     description=description,
